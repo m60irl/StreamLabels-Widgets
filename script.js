6ed39db1acf9c31fa3f05ee5fb69c377628fc963
m60irl <github.mf96t@a.m60.live>
2024-06-04 06:18:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,22 +4,8 @@ const socketToken = 'MY_SOCKET_API_TOKEN';
 // https://streamlabs.com/dashboard#/settings/api-settings API Settings -> API Tokens -> Your API Access Token
 const token = "MY_API_TOKEN";
 
-const labelsList = [
-    'most_recent_follower',
-    'most_recent_cheerer',
-    // 'most_recent_donator',
-    'most_recent_subscriber'
-]
-
-const imagesSRC = [
-    'images/follow.png',
-    'images/bits.png',
-    // 'images/donator.png',
-    'images/sub.png'
-]
-
-// Time in ms for timeOut
-const TIMING = 5000;
+// Use any of the filenames that Streamlabels would normally create
+const labelName = 'session_top_donator';
 
 //Connect to socket
 const streamlabs = io(`https://sockets.streamlabs.com?token=${socketToken}`, {transports: ['websocket']});
@@ -35,7 +21,7 @@ streamlabs.on("disconnect", () => {
 
 streamlabs.on("reconnect", () => {
     console.log("Reconnected to socket");
-    refreshReconnect(0)
+    refreshReconnect()
 });
 
 streamlabs.on('event', (eventData) => {
@@ -43,85 +29,65 @@ if (eventData.type == 'streamlabels') {
     var json = eventData.message["data"];
     console.log(json);
     // Check if there is already an element with class
-    var labelNum = 0;
-    for (var i = 0; i < labelsList.length; i++) {
-        var labelName = labelsList[labelNum];
-        this[labelName] = json[`${labelName}`];
-        text = this[labelName];
-        if (text != "") {
-            // If Element have this class, modify the child element
-            if (document.querySelector("." + labelName) != null) {
-                console.log("Class already exist");
-                editLabel(text, labelName);
-                // Child element does not have same text
-                // if (document.querySelector("." + labelName).innerHTML.includes(text) == false) {
-                //     document.querySelector("." + labelName).innerHTML = `<img src="${imagesSRC[labelNum]}">${text}`;
-                // }
-            }
-            else {
-                // We create the label
-                console.log("Creating label");
-                createLabel(text, labelNum, labelName);
-            } 
+    this[labelName] = json[`${labelName}`];
+    text = this[labelName];
+    if (text != null) {
+        // If Element have this class, modify the child element
+        if (document.querySelector("." + labelName) != null) {
+            console.log("Class already exist");
+            editLabel(text, labelName);
         }
-        labelNum++;
+        else {
+            // We create the label
+            console.log("Creating label");
+            createLabel(text, labelName);
+        } 
     }
 }
 else {
-    console.log("Event without streamlabels type");
+    console.log("Event without streamlabels type");  
     // console.log(eventData.type)
     // console.log(eventData.message["data"])
 }
 });
 
-function init(labelNum) {
+function init() {
     fetch(`https://streamlabs.com/api/v5/stream-labels/files?token=${token}`)
         .then(response=>response.json())
         .then(data=> {
             json = data['data'];
-            for (var i = 0; i < labelsList.length; i++) {
-                var labelName = labelsList[labelNum];
-                this[labelName] = json[`${labelName}`];
-                if (this[labelName] != "") {
-                    text = this[labelName];
-                    createLabel(text, labelNum, labelName);
-                }
-                labelNum++;
+            this[labelName] = json[`${labelName}`];
+            if (this[labelName] != null) {
+                text = this[labelName];
+                createLabel(text, labelName);
             }
-            labelNum = 0;
-            setTimeout(loop, TIMING);
         })
         .catch(function (error) {
             console.log("Error fetching data:", error);
             // Restart
-            setTimeout(init,TIMING,labelNum)
+            setTimeout(init, 5000);
         });
 }
 
-function refreshReconnect(labelNum) {
+function refreshReconnect() {
     fetch(`https://streamlabs.com/api/v5/stream-labels/files?token=${token}`)
         .then(response=>response.json())
         .then(data=> {
             json = data['data'];
-            for (var i = 0; i < labelsList.length; i++) {
-                var labelName = labelsList[labelNum];
-                this[labelName] = json[`${labelName}`];
-                if (this[labelName] != "") {
-                    text = this[labelName];
-                    editLabel(text, labelName);
-                }
-                labelNum++;
+            this[labelName] = json[`${labelName}`];
+            if (this[labelName] != null) {
+                text = this[labelName];
+                editLabel(text, labelName);
             }
-            labelNum = 0;
         })
         .catch(function (error) {
             console.log("Error fetching data:", error);
             // Restart
-            setTimeout(refreshReconnect, TIMING, labelNum);
+            setTimeout(refreshReconnect, 5000);
         });
 }
 
-function createLabel(text, labelNum, labelName){
+function createLabel(text, labelName){
     // Create li element with labelName as class
     let liItem = document.createElement("li");
     liItem.classList.add(labelName);
@@ -131,15 +97,6 @@ function createLabel(text, labelNum, labelName){
     textContainer.innerHTML = `${text}`;
     liItem.appendChild(textContainer);
     document.querySelector('ul').append(liItem);
-    // Create Images
-    addImages(labelNum, labelName);
-}
-
-function addImages(labelNum, labelName){
-    let digits = document.querySelector("." + labelName)
-    let imgItem = document.createElement('img');
-    imgItem.src = `${imagesSRC[labelNum]}`;
-    digits.prepend(imgItem); 
 }
 
 function editLabel(text, labelName) {
@@ -149,9 +106,4 @@ function editLabel(text, labelName) {
     }
 }
 
-function loop() {
-    document.querySelector("ul").append(document.querySelector("ul").firstElementChild)
-    setTimeout(loop, TIMING);
-}
-
-init(0);
+init();
